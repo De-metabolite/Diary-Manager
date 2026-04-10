de501d5c8573f47f7b0d4cc99ba2927768d01f0e
Bello Qudus <belloqudus3@gmail.com>
2026-04-10 03:46:51 -0700

    Full Code of the Diary Manager

---
+++ DiaryEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiaryManager
+{
+    public class DiaryEntry
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+    }
+}
